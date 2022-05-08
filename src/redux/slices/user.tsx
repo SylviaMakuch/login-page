c6cd4178f284d5e01c0e4f48c76994a3c5be9e55
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-05-08 16:04:22 -0700

    added slice for user

---
+++ src/redux/slices/user.tsx
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export const userSlice = createSlice({
+    name: "user",   // name of the slice
+    initialState: {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    } as UserState,
+    reducers: {
+        setName: (state, action: PayloadAction<string>) => {
+            state.name = action.payload;
+        }   as (state: UserState, action: PayloadAction<string>) => void,
+        setEmail: (state, action: PayloadAction<string>) => {
+            state.email = action.payload;
+        }   as (state: UserState, action: PayloadAction<string>) => void,
+        setPassword: (state, action: PayloadAction<string>) => {
+            state.password = action.payload;
+        }   as (state: UserState, action: PayloadAction<string>) => void,
+        setConfirmPassword: (state, action: PayloadAction<string>) => {
+            state.confirmPassword = action.payload;
+        }   as (state: UserState, action: PayloadAction<string>) => void,
+    }   
+});
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -5,3 +5,7 @@ export const store = configureStore({
         user: userReducer,
     }
 });
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
