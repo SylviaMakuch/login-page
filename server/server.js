31b2df1e13d7a909bf22208cd226a43f006b5e2a
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-05-12 22:54:48 -0700

    userRouter made and added to server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import userRouter from "./routes/userRouter.js";
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+import userRouter from "./routes/user.js";
 
 const CONNECTION_URL = 'mongodb+srv://sylviamakowiec:<password>@cluster0.z6ydo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 8000;
