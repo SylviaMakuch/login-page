dee7beb6877af28f2fb5617aaf830d89bfb0f432
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-05-12 22:34:37 -0700

    added mongoDB to

--- server/server.js
+++ server/server.js
@@ -1,19 +1,24 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-app.set('view-engine.ejs');
 app.listen(8000);
 
-app.get('/login', (req, res) => {
-    res.render('login.ejs')
-  })
+const CONNECTION_URL = 'mongodb+srv://sylviamakowiec:<password>@cluster0.z6ydo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
-app.get('/signup', (req, res) => {
-    res.render('signup.ejs')
-  })
+// app.get('/login', (req, res) => {
+//     res.render('login.ejs')
+//   })
+
+// app.get('/signup', (req, res) => {
+//     res.render('signup.ejs')
+//   })
   
   
-  // POST method route
-  app.post('/', (req, res) => {
-    res.send('POST request to the homepage')
-  })
+//   // POST method route
+//   app.post('/', (req, res) => {
+//     res.send('POST request to the homepage')
+//   })
