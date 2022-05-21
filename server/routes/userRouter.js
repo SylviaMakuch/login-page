304a18fb0400b20310c8686848becc255db786e7
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-05-20 18:20:36 -0700

    connected to mongodb!

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // import { signin, signup } from "../controllers/user.js";
 
-router.post("/signin", signin);
+router.post("/login", signin);
 router.post("/signup", signup);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 const PORT = process.env.PORT|| 8000;
 
-const CONNECTION_URL = 'mongodb+srv://sylviamakowiec:<password>@cluster0.z6ydo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://sylviamakowiec:wbvLe3YMnPgwpuMT@cluster0.z6ydo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
